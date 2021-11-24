008bcb1c50c0c3199087e42fad1b65ffce692457
MSshare <71391924+MSshare@users.noreply.github.com>
2021-11-24 18:54:27 +0800

    Update 02-linear-regression-scratch.py

--- ch02/02-linear-regression-scratch.py
+++ ch02/02-linear-regression-scratch.py
@@ -1,21 +1,17 @@
 import random
-
 import torch
 
 ## with torch.no_grad() 则主要是用于停止autograd模块的工作，
 ## 以起到加速和节省显存的作用，具体行为就是停止gradient计算，从而节省了GPU算力和显存，但是并不会影响dropout和batchnorm层的行为。
-
 ## mm只能进行矩阵乘法,也就是输入的两个tensor维度只能是( n × m ) (n\times m)(n×m)和( m × p ) (m\times p)(m×p)
 ## bmm是两个三维张量相乘, 两个输入tensor维度是( b × n × m )和( b × m × p ), 第一维b代表batch size，输出为( b × n × p )
 ## matmul可以进行张量乘法, 输入可以是高维.
-
 ## python知识补充：
 ## Python3 range() 函数返回的是一个可迭代对象（类型是对象），而不是列表类型， 所以打印的时候不会打印列表。
 ## Python3 list() 函数是对象迭代器，可以把range()返回的可迭代对象转为一个列表，返回的变量类型为列表。
 ## Python3 range(start, stop[, step])
 ## Python3 shuffle() 方法将序列的所有元素随机排序。shuffle()是不能直接访问的，需要导入 random 模块。举例：random.shuffle (list)
 ## Python3 yield是python中的生成器
-
 
 ## 人造数据集
 def create_data(w, b, nums_example):
@@ -25,11 +21,9 @@ def create_data(w, b, nums_example):
     y += torch.normal(0, 0.01, y.shape)  # 加入噪声
     return X, y.reshape(-1, 1)  # y从行向量转为列向量
 
-
 true_w = torch.tensor([2, -3.4])
 true_b = 4.2
 features, labels = create_data(true_w, true_b, 1000)
-
 
 ## 读数据集
 def read_data(batch_size, features, lables):
@@ -40,7 +34,6 @@ def read_data(batch_size, features, lables):
         index_tensor = torch.tensor(indices[i: min(i + batch_size, nums_example)])
         yield features[index_tensor], lables[index_tensor]  # 通过索引访问向量
 
-
 batch_size = 10
 for X, y in read_data(batch_size, features, labels):
     print("X:", X, "\ny", y)
@@ -50,17 +43,14 @@ for X, y in read_data(batch_size, features, labels):
 w = torch.normal(0, 0.01, size=(2, 1), requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
 
-
 # 定义模型
 def net(X, w, b):
     return torch.matmul(X, w) + b
-
 
 # 定义损失函数
 def loss(y_hat, y):
     # print("y_hat_shape:",y_hat.shape,"\ny_shape:",y.shape)
     return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2  # 这里为什么要加 y_hat_shape: torch.Size([10, 1])  y_shape: torch.Size([10])
-
 
 # 定义优化算法
 def sgd(params, batch_size, lr):
@@ -68,7 +58,6 @@ def sgd(params, batch_size, lr):
         for param in params:
             param -= lr * param.grad / batch_size  ##  这里用param = param - lr * param.grad / batch_size会导致导数丢失， zero_()函数报错
             param.grad.zero_()  ## 导数如果丢失了，会报错‘NoneType’ object has no attribute ‘zero_’
-
 
 # 训练模型
 lr = 0.03
@@ -84,5 +73,4 @@ for epoch in range(0, num_epochs):
     with torch.no_grad():
         train_l = loss(net(features, w, b), labels)
         print("w {0} \nb {1} \nloss {2:f}".format(w, b, float(train_l.mean())))
-
 print("w误差 ", true_w - w, "\nb误差 ", true_b - b)
